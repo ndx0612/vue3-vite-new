b314bdebea92146e76b16a46b53dfaf8124b4f99
ndx2527 <1378362527@qq.com>
2022-05-14 12:52:12 +0800

    安装sass和页面初始化

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
-  <el-button type="primary">Primary</el-button>
+  <div class="content">
+    123
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
 </script>
+
+
+<style lang="scss">
+.content {
+  width: 100vw;
+  height: 100vh;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  font-size: 14px;
+  background: #fafafa;
+  font-family: MicrosoftYaHei;
+  color: #303133;
+}
+
+* {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+</style>
